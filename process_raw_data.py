cdf0cccb5297cbb7a57d31e35f9cfde428505bff
Paula <trainee.paula.uusnakki@sigmalabs.co.uk>
2024-09-27 14:35:58 +0100

    Clean title

--- process_raw_data.py
+++ process_raw_data.py
@@ -3,9 +3,15 @@
 import argparse
 import os
 import sqlite3
+import re
 import pandas as pd
 
 
+def clean_title(title):
+    """Remove any text in brackets from the title."""
+    if pd.isna(title):
+        return ''
+    return re.sub(r'\s*\[.*?\]\s*|\s*\(.*?\)\s*', '', title).strip()
 
 if __name__ == "__main__":
-    ...
+    print(clean_title("Five Feet Apart(ebook)"))
