4aff68ee290f9b770d42498414522869e2cb8c4e
Paula <trainee.paula.uusnakki@sigmalabs.co.uk>
2024-09-27 15:34:16 +0100

    Loading data

--- analyse_processed_data.py
+++ analyse_processed_data.py
@@ -4,5 +4,13 @@ import pandas as pd
 import altair as alt
 import os
 
+
+def load_processed_data(file_path):
+    """Load the processed data from the given CSV file."""
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"{file_path} not found.")
+    return pd.read_csv(file_path)
+
 if __name__ == "__main__":
-    ...
+    
+    print(load_processed_data("data/PROCESSED_DATA.csv"))
